aca2fa92fdc8b68f20ab23755657a3831e87c3a3
Critianr <calrod350@gmail.com>
2024-03-16 10:35:51 -0500

    hi

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,20 +1,39 @@
 // MenuButton.js
+import Button from 'react-bootstrap/Button';
+import Offcanvas from 'react-bootstrap/Offcanvas';
 import React, { useState } from 'react';
 import './menu.css';
 
 function MenuButton() {
-  const [isOpen, setIsOpen] = useState(false);
+    // OffCanvasExample
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+//   const [isOpen, setIsOpen] = useState(false);
 
-  const toggleMenu = () => {
-    setIsOpen(!isOpen);
-  };
+//   const toggleMenu = () => {
+//     setIsOpen(!isOpen);
+//   };
 
   return (
-    <div className={`MenuButton ${isOpen ? 'open' : ''}`} onClick={toggleMenu}>
+    <>
+    {/* <div className={`MenuButton ${isOpen ? 'open' : ''}`} onClick={toggleMenu}> */}
+      <div onClick={handleShow} className={`MenuButton ${show ? 'open' : ''}`}>
       <div className="bar1"></div>
       <div className="bar2"></div>
       <div className="bar3"></div>
     </div>
+     <Offcanvas show={show} onHide={handleClose}>
+     <Offcanvas.Header closeButton>
+       <Offcanvas.Title>2MG SOLUTIONS</Offcanvas.Title>
+     </Offcanvas.Header>
+     <Offcanvas.Body>
+       <div>
+         Contenido del menú aquí
+       </div>
+     </Offcanvas.Body>
+   </Offcanvas>
+   </>
   );
 }
 
